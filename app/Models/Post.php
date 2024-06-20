7320b0933fbe51a3d96185122c7ff4cc4e43c361
Krisdian Widya Ekatama <krisdianwidya@gmail.com>
2024-06-20 12:19:03 +0700

    add migration and model

---
+++ app/Models/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Post extends Model
+{
+    protected $table = 'posts';
+    protected $primaryKey = 'id';
+    protected $useSoftDeletes = true;
+    protected $allowedFields = ['title', 'body'];
+    protected $useTimestamps = true;
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
+}
